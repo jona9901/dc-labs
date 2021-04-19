bfab6474a5cea7d2daaba8dfdee0b5c920a73efa
Jonathan Castillo <castillojonathan068@gmail.com>
2021-04-19 18:29:56 -0500

    final version

--- challenges/second-partial/server.go
+++ challenges/second-partial/server.go
@@ -6,20 +6,9 @@ import (
 	"io"
 	"time"
 	"net/http"
-	//"strings"
 	"github.com/gin-gonic/gin"
 	"github.com/dgrijalva/jwt-go"
 )
-
-/*
-var routes = [] route {
-	newRoute("GET", "/", home),
-	newRoute("GET", "/login", login),
-	newRoute("GET", "/logout", logout),
-	newRoute("GET", "/upload", upload),
-	newRoute("GET", "/status", status),
-}
-*/
 
 type User struct {
 	Username string
@@ -28,7 +17,7 @@ type User struct {
 }
 
 var loggedUsers []*User
-//var files []*os.File
+
 /*
 A sample user
 */
@@ -38,6 +27,7 @@ var defaultUser = User {
 	Password: "password",
 }
 
+// new user recives the username and the password and returns a user struct
 func newUser(username string, password string) *User{
 	token, _ := createToken(username)				// handle error
 
@@ -49,6 +39,7 @@ func newUser(username string, password string) *User{
 	return &u
 }
 
+// recives the username and returns a token (string) and an error if exists, if not nil
 func createToken(username string) (string, error) {
 	var err error
 
@@ -66,9 +57,9 @@ func createToken(username string) (string, error) {
 	return token, nil
 }
 
-func login(c *gin.Context) {						// asign token
-	username, password, _ := c.Request.BasicAuth()				// If needed Basic auth returns: (username, password string, ok bool)
-
+// login API endpoint
+func login(c *gin.Context) {
+	username, password, _ := c.Request.BasicAuth()
 	if username != defaultUser.Username || password != defaultUser.Password {
 		c.JSON(http.StatusUnauthorized, "Please provide valid credentials")
 		return
@@ -85,10 +76,12 @@ func login(c *gin.Context) {						// asign token
 	})
 }
 
+// struct tu recieve the token from the header
 type tokenHeader struct {
 	Token string `header:"Authorization"`
 }
 
+// logout API endpoint
 func logout(c *gin.Context) {
 	h := tokenHeader{}
 
@@ -113,15 +106,13 @@ func logout(c *gin.Context) {
 		})
 	}
 }
-/*
-type uploadBody struct {
-	Body string `form:"data"`
-}
-*/
+
+// struct to recieve the data (file to upload path) from the body
 type uploadBody struct {
 	Body string `content-type: "JSON" form:"data"`
 }
 
+// upload API endpoint
 func upload(c *gin.Context) {
 	h := tokenHeader{}
 	//data := uploadBody{}
@@ -130,91 +121,6 @@ func upload(c *gin.Context) {
 		c.JSON(700, err)						// err 700 -> header error
 		return
 	}
-	/*
-
-	if err := c.ShouldBind(&data); err != nil {
-		c.JSON(800, err)						// err 800 -> body error
-		return
-	}
-	*/
-
-/*
-*
-*
-*
-*/
-
-/*
-	multipartFile, err := c.FormFile("data")
-
-	if err != nil {
-		c.JSON(100, gin.H {				// 100 -> file upload error
-			"message": err,
-		})
-	}
-
-	f, err := multipartFile.Open()
-
-	if err != nil {
-		c.JSON(104, gin.H {
-			"message": err,				// 104 -> file open error
-		})
-	}
-
-	newFile, err := os.Create("./imgs/" + multipartFile.Filename)
-
-	if err != nil {
-		c.JSON(101, gin.H {				// 101 -> file create error
-			"message": err,
-		})
-		return
-	}
-
-	if _, err := io.Copy(newFile, f); err != nil {
-		c.JSON(102, gin.H {				// 102 -> file copy error
-			"message": err,
-		})
-		return
-
-	}
-
-	fi, err := newFile.Stat()
-	if err != nil {
-		c.JSON(103, gin.H {				// 103 -> file size error
-			"message": err,
-		})
-		return
-	}
-
-	size := fmt.Sprintf("%d bytes", fi.Size())
-
-	c.JSON(200, gin.H {
-		"message": "An image has been successfully uploaded",
-		"filename": multipartFile.Filename,				// add the filename variable
-		"size": size,
-	})
-
-	f.Close()
-	newFile.Close()
-	return
-*/
-
-
-
-/*
-	if err := c.ShouldBind(&data); err != nil {
-		c.JSON(800, err)						// err 800 -> body error
-		return
-	}
-
-	fmt.Println(data.Body)
-*/
-/*
-*
-*
-*
-*/
-
 
 	for _, u := range loggedUsers {
 		if h.Token == u.Token {
@@ -224,6 +130,7 @@ func upload(c *gin.Context) {
 				c.JSON(100, gin.H {				// 100 -> file upload error
 					"message": err,
 				})
+				return
 			}
 
 			f, err := multipartFile.Open()
@@ -232,6 +139,7 @@ func upload(c *gin.Context) {
 				c.JSON(104, gin.H {
 					"message": err,				// 104 -> file open error
 				})
+				return
 			}
 
 			newFile, err := os.Create("./imgs/" + multipartFile.Filename)
@@ -263,78 +171,22 @@ func upload(c *gin.Context) {
 
 			c.JSON(200, gin.H {
 				"message": "An image has been successfully uploaded",
-				"filename": multipartFile.Filename,				// add the filename variable
+				"filename": multipartFile.Filename,
 				"size": size,
 			})
 
 			f.Close()
 			newFile.Close()
 			return
-
 		}
 	}
 
-/*
-	for _, u := range loggedUsers {
-		if h.Token == u.Token {
-			c.Request.ParseMultipartForm(32 << 20)
-
-			f, err := os.Open(data.Body)
-			if err != nil {
-				c.JSON(100, gin.H {				// 100 -> file upload error
-					"message": err,
-				})
-				return
-			}
-
-			defer f.Close()
-
-			body := strings.Split(data.Body, "/")
-			filename := body[len(body) - 1]
-
-			newFile, err := os.Create("./imgs/" + filename)//fmt.Sprintf("/imgs/%s", filename))
-
-			if err != nil {
-				c.JSON(101, gin.H {				// 101 -> file create error
-					"message": err,
-				})
-				return
-			}
-
-			if _, err := io.Copy(newFile, f); err != nil {
-				c.JSON(102, gin.H {				// 102 -> file copy error
-					"message": err,
-				})
-				return
-
-			}
-
-			//files = append(files, newFile)
-
-			fi, err := f.Stat()
-			if err != nil {
-				c.JSON(103, gin.H {				// 103 -> file size error
-					"message": err,
-				})
-				return
-			}
-
-			size := fmt.Sprintf("%d.kb", fi.Size())
-
-			c.JSON(200, gin.H {
-				"message": "An image has been successfully uploaded",
-				"filename": filename,				// add the filename variable
-				"size": size,
-			})
-			return
-		}
-	}
-*/
 	c.JSON(500, gin.H {					// err 500 -> bad token
 		"message": "Error, not logged in",
 	})
 }
 
+// status API enpoint
 func status(c *gin.Context) {
 	h := tokenHeader{}
 
@@ -359,6 +211,7 @@ func status(c *gin.Context) {
 	})
 }
 
+// main function
 func main() {
 	fmt.Printf("Starting server at port 8080\n")
 
@@ -373,3 +226,17 @@ func main() {
 
 	server.Run()
 }
+
+/*
+errors
+
+err 100 -> file upload error
+err 101 -> file create error
+err 102 -> file copy error
+err 103 -> file size error
+err 104 -> file open error
+err 500 -> bad token
+err 700 -> header error
+
+
+*/
